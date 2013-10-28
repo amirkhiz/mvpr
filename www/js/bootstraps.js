aeeabe62354a18694d136c5b08bef17f43815e82
sina.saderi <sina.saderi@d06f649f-bbdf-569f-2037-1f9c3e28b809>
2013-10-28 06:25:20 +0000

    searxh txt and full text search is ok

--- www/js/bootstraps.js
+++ www/js/bootstraps.js
@@ -1,22 +1,55 @@
 $(document).ready(function() {
 	
-	$('body').on('click', '.catUl > li > span', function () {
+	$("body").click(function(){
+		searchBtnDivClose();
+		searchTxtDivClose();
+	});
+	
+	$('body').on('click', '.catUl > li > span', function (e) {
+		e.stopPropagation();
 		catId = $(this).attr("cat-id");
 		loadCategory(catId);
 		
 	});
 	
-    $("#searchBtn").click(function(){
+	/*$("#searchTxtBox, #searchTxtDiv, #searchBtnDiv").click(function(e){
+		e.stopPropagation();
+	});
+	*/
+	
+	$("#searchTxtBox").keyup(function(e){
+			searchTxtDivOpen();
+			keys = $(this).val();
+			searchCategory(keys);
+	});
+	
+    $("#searchBtn").click(function(e){
+    	e.stopPropagation();
     	if($("#searchBtnGly").hasClass("glyphicon-chevron-down")){
-    		serachBtnDivOpen();
+    		searchBtnDivOpen();
     		loadCategory(0);
     	}else{
-    		serachBtnDivClose();
+    		searchBtnDivClose();
     	}
     });
     
-    function loadCategory(categoryId){
-    	//console.log(categoryId);
+    function searchCategory(keys){
+    	var ajaxurl  = makeUrl({module: "category", action: "searchTxtList"});
+		$.ajax({
+			url: ajaxurl,
+            type: 'POST',
+            data: {keys: keys},
+            async:false,
+			success: function(data){
+            	$("#searchTxtDiv").html(data);
+            },
+			error: function(){
+				console.log('Error');
+			}
+		});
+    }
+    
+    function loadCategory(categoryId,e){
     	var ajaxurl  = makeUrl({module: "category", action: "searchBtnList"});
 		$.ajax({
 			url: ajaxurl,
@@ -30,7 +63,7 @@ $(document).ready(function() {
 
 				if(aData[3] == ""){
 					nextLevel = parseInt(aData[2]) + parseInt(1);
-					$(".cat-list-"+nextLevel).html("");
+					$(".cat-list-"+nextLevel+" .inner").html("");
 					hasChild = 0;
 				}
 				
@@ -38,11 +71,11 @@ $(document).ready(function() {
 					if(hasChild){
 						n = parseInt(aData[0]) + 1;
 						for(i = n; i <= 4; i++){
-							$(".cat-list-"+i).html("");
+							$(".cat-list-"+i+" .inner").html("");
 						}
 					}
 					
-					$(".cat-list-"+aData[0]).html(aData[1]);
+					$(".cat-list-"+aData[0]+" .inner").html(aData[1]);
 					
 				}
 				
@@ -54,7 +87,32 @@ $(document).ready(function() {
 		});
     }
     
-    function serachBtnDivOpen(){
+    txtOpen = 0;
+    function searchTxtDivOpen(){
+    	txtOpen = 1;
+    	$("#searchTxtDiv").css("display","block");
+    	//$("#btn-inner").fadeIn("slow");
+    	$("#searchTxtDiv")
+    	.animate({ height: "300px" }, 100 )
+        .animate({ width: "1000px" }, 200 );
+    	
+    	$("#searchTxtBox").animate({backgroundColor: "#444444"}, 300);
+    }
+    
+    function searchTxtDivClose(){
+    	txtOpen = 0;
+    	//$("#btn-inner").fadeOut("fast");
+    	$("#searchTxtBox").animate({backgroundColor: "#222222"}, 300);
+    	$("#searchTxtDiv")
+    	.animate({ width: "0px" }, 200 )
+    	.animate({ height: "0px" }, 100 )
+    	$("#searchTxtDiv").fadeOut("fast");
+    }
+    
+    function searchBtnDivOpen(){
+    	if(txtOpen == 1){
+    		searchTxtDivClose();
+    	}
     	$("#searchBtnDiv").css("display","block");
     	$("#btn-inner").fadeIn("slow");
     	$("#searchBtnDiv")
@@ -65,9 +123,9 @@ $(document).ready(function() {
     	$("#searchBtnGly").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
     }
     
-    function serachBtnDivClose(){
+    function searchBtnDivClose(){
     	$("#searchBtnGly").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
-    	$("#btn-inner").fadeOut("slow");
+    	$("#btn-inner").fadeOut("fast");
     	$(".searchBtnInner").animate({backgroundColor: "#f86d18"}, 300);
     	$("#searchBtnDiv")
     	.animate({ width: "0px" }, 200 )
