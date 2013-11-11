ced7a91a00f5003a6cb22f0f351db69001b6ff1c
sina.saderi <sina.saderi@d06f649f-bbdf-569f-2037-1f9c3e28b809>
2013-11-11 12:25:39 +0000

    add value field for content_addition table

--- trunk/var/cache/entities/Content_addition.php
+++ trunk/var/cache/entities/Content_addition.php
@@ -13,6 +13,7 @@ class DataObjects_Content_addition extends DB_DataObject
     public $content_addition_id;             // int(11)  not_null primary_key
     public $product_id;                      // int(11)  not_null
     public $content_type_mapping_data_id;    // int(11)  not_null
+    public $value;                           // string(255)  not_null
 
     /* Static get */
     function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DataObjects_Content_addition',$k,$v); }
